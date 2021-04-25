ca5872ac5aa9c08e1cfee4b6c0ba6dab431ea944
f4br1c10 <fabriciopstos@hotmail.com>
2021-04-24 23:25:05 -0300

    Aula 03 - In Orbit

---
+++ src/components/Header.tsx
@@ -0,0 +1,53 @@
+import React from 'react'
+import {
+    View,
+    StyleSheet,
+    Text,
+    Image
+} from 'react-native'
+import { getStatusBarHeight } from 'react-native-iphone-x-helper'
+
+import userImg from '../assets/fabricio.png'
+import colors from '../styles/colors'
+import fonts from '../styles/fonts'
+
+export function Header() {
+    return (
+        <View style={styles.container}>
+            <View>
+                <Text style={styles.greeting}>Olá,</Text>
+                <Text style={styles.userName}>Fabricio</Text>
+            </View>
+
+            <Image source={userImg} style={styles.image} />
+
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    container: {
+        width: '100%',
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        paddingVertical: 20,
+        marginTop: getStatusBarHeight(),
+    },
+    image: {
+        width: 60,
+        height: 60,
+        borderRadius: 30
+    },
+    greeting: {
+        fontSize: 32,
+        color: colors.heading,
+        fontFamily: fonts.text
+    },
+    userName: {
+        fontSize: 32,
+        fontFamily: fonts.heading,
+        color: colors.heading,
+        lineHeight: 40
+    }
+})
